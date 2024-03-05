bf0855447ad23102aa83fedfbe03fb41dcab9fcb
H82M19\Student <Student>
2024-03-05 08:21:15 +0700

    Bai1,2

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, Animated, TouchableOpacity } from 'react-native';
+import { Button } from 'react-native-web';
 
-export default function App() {
+const App = () => {
+
+  const [fadeAnim] = useState(new Animated.Value(0));
+  const [moving, setMoving] = useState(false);
+  const position = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
+
+  useEffect(() => {
+    Animated.timing(
+      fadeAnim,
+      {
+        toValue: 1,
+        duration: 3000,
+        useNativeDriver: true,
+      }
+    ).start();
+  }, []);
+
+ 
+  const startMoving = () => {
+    Animated.timing(position, {
+      toValue: { x: 200, y: 200 },
+      duration: 1000,
+      useNativeDriver: false,
+    }).start();
+    setMoving(true);
+  };
+  
+  const restartMoving = () => {
+    position.setValue({ x: 0, y: 0 });
+    setMoving(false);
+  };
+
+  const stopMoving = () => {
+    position.stopAnimation((value) => {
+      startPosition.x = value.x;
+      startPosition.y = value.y;
+    });
+    setMoving(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Animated.Text style={{ opacity: fadeAnim }}>
+        20101791_VoMinhAnh
+      </Animated.Text>
+      <Animated.Text style={{ opacity: fadeAnim }}>
+        Moblie Nang cao
+      </Animated.Text>
+
+      <Animated.View style={{ transform: position.getTranslateTransform() }}>
+        <View style={{ width: 100, height: 100, backgroundColor: 'red' }} />
+      </Animated.View>
+      <Button title="Start" onPress={startMoving} />
+      <Button title="Stop" onPress={stopMoving} />
+      <Button title="Restart" onPress={restartMoving} />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
